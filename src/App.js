a1e1b54840d606f0f31e281458304a9549239d58
KristynaCharova <78442699+KristynaCharova@users.noreply.github.com>
2021-05-02 19:58:55 +0200

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,85 +1,12 @@
 import "./App.css";
-import Forecast from "./Forecast";
+
 import Weather from "./Weather";
-import ReactAnimatedWeather from "react-animated-weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
-        <div className="row">
-          <div className="col-sm-2">
-            <Forecast
-              temp={17}
-              icon={
-                <ReactAnimatedWeather
-                  icon="CLEAR_DAY"
-                  color="black"
-                  size="45"
-                  animate={true}
-                />
-              }
-              day="Sunday"
-            />
-          </div>
-          <div className="col-sm-2">
-            <Forecast
-              temp={12}
-              icon={
-                <ReactAnimatedWeather
-                  icon="PARTLY_CLOUDY_DAY"
-                  color="black"
-                  size="45"
-                  animate={true}
-                />
-              }
-              day="Monday"
-            />
-          </div>
-          <div className="col-sm-2">
-            <Forecast
-              temp={15}
-              icon={
-                <ReactAnimatedWeather
-                  icon="WIND"
-                  color="black"
-                  size="45"
-                  animate={true}
-                />
-              }
-              day="Tuesday"
-            />
-          </div>
-          <div className="col-sm-2">
-            <Forecast
-              temp={10}
-              icon={
-                <ReactAnimatedWeather
-                  icon="SLEET"
-                  color="black"
-                  size="45"
-                  animate={true}
-                />
-              }
-              day="Wednesday"
-            />
-          </div>
-          <div className="col-sm-2">
-            <Forecast
-              temp={8}
-              icon={
-                <ReactAnimatedWeather
-                  icon="FOG"
-                  color="black"
-                  size="45"
-                  animate={true}
-                />
-              }
-              day="Thursday"
-            />
-          </div>
-        </div>
+        <Weather defaultCity="Prague" />
       </div>
       <footer>
         Open-sourced projected on{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Form from "./Form";
 
 import "./Weather.css";
+import Loader from "react-loader-spinner";
 
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Prague",
-    temperature: 12,
-    date: "Saturday 18:35",
-    description: "Rain",
-    wind: 5,
-    humidity: 85,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  return (
-    <div className="Weather">
-      <Form />
-      <div className="row">
-        <div className="col-6">
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>{weatherData.date}</li>
-            <li>{weatherData.description}</li>
-          </ul>
-          <ul>
-            <li>
-              Wind: <span>{weatherData.wind}</span> m/h
-            </li>
-            <li>
-              Humidity: <span>{weatherData.humidity}</span>%
-            </li>
-          </ul>
-        </div>
-        <div className="col-6 main-icon">
-          <ReactAnimatedWeather
-            icon="RAIN"
-            color="#000"
-            size={120}
-            animate={true}
-          />
-          <span className="temperature">{weatherData.temperature}</span>
-          <strong>°C</strong>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      date: "Saturday 18:35",
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <Form />
+        <div className="row">
+          <div className="col-6">
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+            </ul>
+            <ul>
+              <li>
+                Wind: <span>{weatherData.wind}</span> m/h
+              </li>
+              <li>
+                Humidity: <span>{weatherData.humidity}</span>%
+              </li>
+            </ul>
+          </div>
+          <div className="col-6 main-icon">
+            <ReactAnimatedWeather
+              icon="RAIN"
+              color="#000"
+              size={120}
+              animate={true}
+            />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <strong>°C</strong>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "087811ad20c5b942f2408cdd748d2b31";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return (
+      <Loader
+        type="Hearts"
+        color="#E27B83"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
+  }
 }
